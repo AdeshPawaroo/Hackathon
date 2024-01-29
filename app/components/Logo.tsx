c61a21e6ea4c2fec3bb8e63f775db0df8020358a
Jordy <jordycorporan95@gmail.com>
2024-01-29 17:36:46 -0500

    add navbar

---
+++ app/components/Logo.tsx
@@ -0,0 +1,11 @@
+interface LogoProps {
+    src: string;
+    alt?: string;
+    height?: string;
+}
+
+const Logo: React.FC<LogoProps> = ({ src, alt = 'Logo', height = '50px' }) => {
+    return <img src={src} alt={alt} style={{ height }} />;
+};
+
+export default Logo;
