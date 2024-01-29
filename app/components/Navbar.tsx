c99b1b211be943e5fbbcc8377b3a43e5d2519dd2
Jordy <jordycorporan95@gmail.com>
2024-01-29 18:07:56 -0500

    add testimonials

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,12 +13,13 @@ const Navbar: React.FC = () => {
 
     return (
         <nav className="flex items-center justify-between h-20 px-4 bg-teal-500">
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-3">
 
                 <Logo />
                 <ul className="flex space-x-8">
                     <MenuItem href="/about" label="About Jada" />
                     <MenuItem href="/services" label="Services" />
+                    <MenuItem href="/testimonials" label="Testimonials" />
                     <MenuItem href="/book" label="Book Jada" />
                     <MenuItem href="/resources" label="Resources" />
                     <MenuItem href="/contact" label="Contact" />
