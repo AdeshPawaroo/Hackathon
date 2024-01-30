3ad29057b2a733446d248c44cc2175f2317c8566
Sheena <aries.sheen@gmail.com>
2024-01-30 11:58:31 -0800

    Added context file and some changes in Layout page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,4 @@
 
-// import Context from './components/Context/context'
-// import { AppProps } from 'next/app';
-
-// export default function Page({ Component, pageProps }: AppProps) {
-//   return(
-//     <div>
-//     <Context>
-//       <Component {...pageProps} />
-//     </Context>
-//     </div>
-//   )
-// }
 export default function Page() {
   return(
     <div>
