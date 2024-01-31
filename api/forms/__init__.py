9ade06dbbf1246c79142bb5d39b306bb2a1cd0da
Sheena <aries.sheen@gmail.com>
2024-01-31 14:44:44 -0800

    Added message context on Herocard

--- api/forms/__init__.py
+++ api/forms/__init__.py
@@ -2,3 +2,4 @@ from .booking_form import BookingForm
 from .testimonial_form import TestimonialForm
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .home_page_form import HomePageForm
