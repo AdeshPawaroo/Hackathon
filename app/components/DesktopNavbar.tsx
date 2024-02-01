d6c4f72acc79854275ecfc2d25b7cf5e57545817
Tom Leger <tomleger@gmail.com>
2024-02-01 11:14:06 -0500

    added FAQ to navbar

--- app/components/DesktopNavbar.tsx
+++ app/components/DesktopNavbar.tsx
@@ -8,7 +8,8 @@ import Link from 'next/link'
 const menuItems = [
   { href: '/about', label: 'About Jada' },
   { href: '/services', label: 'Services' },
-  { href: '/book', label: 'Book Jada' },
+  { href: '/faq', label: 'FAQ' },
+  { href: '/booking', label: 'Book Jada' },
   { href: '/resources', label: 'Resources' },
   { href: '/contact', label: 'Contact' },
 ]
