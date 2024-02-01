8d72f91e85fbe340aee2ce3212d0ab7278166e28
Tom Leger <tomleger@gmail.com>
2024-02-01 16:49:19 -0500

    updated dashboard pages styling

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import Button from '../components/Button'
 import Sidebar from './components/Sidebar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,21 +14,22 @@ const inter = Inter({ subsets: ['latin'] })
 // }
 
 export default function Layout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
 
 
-    return (
-        <div className={`${inter.className} antialiased container-full mx-auto flex`}>
-        <div className="w-64"> {/* Set fixed width for sidebar */}
-          <Sidebar />
-        </div>
-        <div className="flex-1"> {/* Main content takes up the remaining space */}
-          {children}
-        </div>
+
+  return (
+    <div className={`${inter.className} antialiased container-full mx-auto flex  bg-gray-100`}>
+      <div className="w-64"> {/* Set fixed width for sidebar */}
+        <Sidebar />
       </div>
-      
-      );
+      <div className="flex-1"> {/* Main content takes up the remaining space */}
+        {children}
+      </div>
+    </div>
+
+  );
 }
