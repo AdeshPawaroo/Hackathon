c618c138385d37870de0ec1d3be8c8f9db88d7d4
Sheena <aries.sheen@gmail.com>
2024-02-01 12:02:53 -0800

    Created all models for all pages

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -3,3 +3,9 @@ from .booking import Booking
 from .testimonials import Testimonial
 from .user import User
 from .home_page import HomePage
+from .about_page import AboutPage
+from .contact_page import ContactPage
+from .faq_page import FAQPage
+from .resources_page import ResourcePage
+from .services_page import ServicePage
+from .testimonials_page import TestimonialPage
