3788be16945e9438ca1c0f95d0bbe17531b31a92
Tom Leger <tomleger@gmail.com>
2024-02-02 18:11:32 -0500

    added highlights to active routes in sidebar

--- app/dashboard/components/SidebarItem.tsx
+++ app/dashboard/components/SidebarItem.tsx
@@ -1,18 +1,22 @@
 'use client';
-import React, { useEffect } from 'react';
-import Link from 'next/link';
 
+import React from 'react';
+import Link from 'next/link';
+import clsx from 'clsx';
 
 interface SidebarItemProps {
     href: string;
     label: string;
+    isActive: boolean; // Declare isActive here only
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({ href, label }) => {
-
+const SidebarItem: React.FC<SidebarItemProps> = ({ href, label, isActive }) => {
     return (
         <Link href={href} passHref>
-            <span className={`py-2 px-4 block cursor-pointer mb-2`}>
+            <span className={clsx(
+                "py-2 px-4 block cursor-pointer mb-2",
+                isActive && "bg-jada-blue-400" // Apply this class if isActive is true
+            )}>
                 {label}
             </span>
         </Link>
