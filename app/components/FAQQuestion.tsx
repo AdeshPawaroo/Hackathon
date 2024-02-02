3015c625b5160d3e2315d6a3bcb43018368348ed
Jordy <jordycorporan95@gmail.com>
2024-02-01 23:47:52 -0500

    testing deployment

--- app/components/FAQQuestion.tsx
+++
@@ -1,8 +0,0 @@
-// components/FAQQuestion.tsx
-import React from 'react';
-
-const FAQQuestion = ({ question }) => {
-  return <h2 className="text-xl font-semibold mb-2">{question}</h2>;
-};
-
-export default FAQQuestion;
