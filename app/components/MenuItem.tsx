249f73b76bec60069b072d2ec423a882f9d1f4c7
Jordy <jordycorporan95@gmail.com>
2024-02-03 16:59:25 -0500

    navbar styling

--- app/components/MenuItem.tsx
+++ app/components/MenuItem.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 interface MenuItemProps {
     href: string;
     label: string;
-    isActive: boolean;
+    isActive?: boolean;
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ href, label, isActive }) => {
@@ -14,7 +14,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ href, label, isActive }) => {
             isActive && "bg-jada-blue-500" // Highlight the active link
         )}>
             <Link href={href}>
-                <span className="block px-3 py-2 text-md font-medium">{label}</span>
+                <span className="block px-2 py-2 text-md font-medium">{label}</span>
             </Link>
         </li>
     );
