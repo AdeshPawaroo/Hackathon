d280c4434e5241b2dd8229d5d968b333615d45e2
Tom Leger <tomleger@gmail.com>
2024-02-05 11:29:17 -0500

    adjusted layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,13 +11,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <div className="w-64 bg-gray-100 sticky top-0 h-screen"> {/* Adjusted sidebar styles */}
-        <Sidebar />
-      </div>
-      <div className="flex-1 bg-gray-100 p-4 overflow-auto"> {/* Ensure the main content area also has a grey background */}
+    <div className={`${inter.className} flex min-h-screen bg-gray-100`}>
+    <div className="w-64 bg-gray-100 sticky top-0 h-screen"> {/* Adjusted sidebar styles */}
+      <Sidebar />
+    </div>
+    <div className="flex-1 p-4 overflow-auto"> {/* Main content container */}
+      <div className="max-w-7xl mx-auto bg-gray-100"> {/* Centered container with max-width */}
         {children}
       </div>
     </div>
+  </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,14 @@ export default function RootLayout({
         <body
           className={`${nunito.className} antialiased container-full mx-auto bg-jada-green-500`}
         >
-          {isLoggedIn && <AdminNavbar />}
-          {!pathname.startsWith('/dashboard') && <Navbar />}
-          {children}
+          <div className={`${nunito.className} min-h-screen bg-jada-green-500`}>
+  {isLoggedIn && <AdminNavbar />}
+  {!pathname.startsWith('/dashboard') && <Navbar />}
+  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    {children}
+  </div>
+</div>
+
         </body>
       </html>
     </Context>
